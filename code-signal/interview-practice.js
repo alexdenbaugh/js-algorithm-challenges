b1dba25a0da149c367536727b6209b29a3bfa16d
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-06-16 19:37:16 -0700

    first-not-repeating-character.

--- code-signal/interview-practice.js
+++ code-signal/interview-practice.js
@@ -1,3 +1,22 @@
+function firstNotRepeatingCharacter(s) {
+  const u = [];
+  const no = new Set();
+  for (let i = 0; i < s.length; i++) {
+    if (u.includes(s[i])) {
+      u.splice(u.indexOf(s[i]), 1);
+      if (!no.has(s[i])) {
+        no.add(s[i]);
+      }
+    } else if (!no.has(s[i])) {
+      u.push(s[i]);
+    }
+  }
+  if (!u.length) {
+    return '_';
+  }
+  return u[0];
+}
+
 function firstDuplicate(a) {
   const b = new Set();
   for (let i = 0; i < a.length; i++) {
